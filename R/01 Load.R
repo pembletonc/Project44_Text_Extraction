3e9b04deab0dc912b433ac2c242fddc9e6f0630c
Corey Pembleton <pembletonc@gmail.com>
2019-10-04 08:32:41 -0400

    Single file functions well, now moving onto single-file multi-image

--- R/01 Load.R
+++ R/01 Load.R
@@ -7,17 +7,23 @@ library(magick)
 library(purrr)
 library(openxlsx)
 
+#for one page-----------------------------------------
 
 file <- pdftools::pdf_convert("./test-data/001_0145.pdf", page = 1,
-                              filenames = "page1.png", dpi = 300)
+                              filenames = "./test-data/page1.png", dpi = 250)
 
 # original size
 image <- image_read("./test-data/page1.png")
 
+magick::image_attributes(image)
+
+image %>%
+  image_crop(geometry_area(width = 2200, height = 1600, y_off = 500, x_off = 650)) 
+
 text <- 
   image %>%
-  image_crop(geometry_area(width = 1300, height = 950, y_off = 285, x_off = 380)) %>%  
-  image_resize("3000x") %>%
+  image_crop(geometry_area(width = 2200, height = 1600, y_off = 500, x_off = 650)) %>%  
+  image_resize("2000x") %>%
   image_background("white", flatten = TRUE) %>% 
   image_noise(noisetype = "Uniform") %>%          # Reduce noise in image using a noise peak elimination filter
   image_enhance() %>%                             # Enhance image (minimize noise)
@@ -31,55 +37,91 @@ text <-
 
 (text <- stringr::str_split(text, pattern = "\\s{5,}"))
 
-#date <- 
-  image %>% 
-  image_crop(geometry_area(width = 120, height = 950, y_off = 285, x_off = 200)) %>% 
-    #image_resize("3000x") %>%
-    image_background("white", flatten = TRUE) %>% 
-    #image_noise(noisetype = "Uniform") %>%          # Reduce noise in image using a noise peak elimination filter
-    image_enhance() %>%                             # Enhance image (minimize noise)
-    image_normalize() %>% 
-    image_convert(type = 'Grayscale') %>%
-    image_trim(fuzz = 40) %>%
-    image_contrast(sharpen = 1) %>%
-    #image_deskew(threshold = 40) %>% 
-    image_write(format = 'png', density = '300x300') %>%
-    tesseract::ocr(tesseract(options = list(preserve_interword_spaces = 1,
-                                            textord_tabfind_find_tables = 1,
-                                            textord_tablefind_recognize_tables = 1)))
-
-
-text <- stringr::str_split(text, pattern = "\\s{5,}")
-
-names(text) <- "Text"
-
 #need to set name before 
+names(text) <- "Text"
 
 t <- map_df(text, ~.x)
 
-openxlsx::
+readr::write_file(text, "./test-data/pg1_test1.txt")
+
+print_xlsx <- function(df){
+  
+  wb <- createWorkbook()
+  addWorksheet(wb, "Page 1", gridLines = FALSE)
+  writeData(wb, sheet = 1, t, rowNames = FALSE)
+  saveWorkbook(wb, "test_ocr_conversion.xlsx", overwrite = TRUE)
+  
+  
+}
+
+
+#for single doc with multiple pages---------------------------------------------------
+file_name <- tools::list_files_with_exts(dir = "./test-data", exts = "pdf")
+page_count <- pdf_info(file_name)$pages  
+
+multi_files <- list(pdftools::pdf_convert(file_name, page = 1:page_count,
+                                          filenames = paste0("./test-data/", "page", 1:page_count, ".png"),
+                                          dpi = 250))
+
+#once done the first time can just read in the files
+
+multi_files <- list(tools::list_files_with_exts(dir = "./test-data", exts = "png"))
+
+
+#for multiple pdfs:
+#  map(filelist, FUN = function(file) {
+#    pdf_convert(files, format = "png")
+#    })
+
+multi_images <- map(multi_files, image_read)
+
+multi_images1 <- image_join(multi_images)
+
+multi_text_clean <- function(images){
+  
+  Map(function(x) {
+    x %>% 
+      image_crop(geometry_area(width = 2200, height = 1600, y_off = 500, x_off = 650)) %>%  
+      image_resize("2000x") %>%
+      image_background("white", flatten = TRUE) %>% 
+      image_noise(noisetype = "Uniform") %>%          # Reduce noise in image using a noise peak elimination filter
+      image_enhance() %>%                             # Enhance image (minimize noise)
+      image_normalize() %>% 
+      image_convert(type = 'Grayscale') %>%
+      image_trim(fuzz = 40) %>%
+      image_contrast(sharpen = 1) %>%
+      #image_deskew(threshold = 40) %>% 
+      image_write(format = 'png', density = '300x300') %>%
+      tesseract::ocr(tesseract(options = list(preserve_interword_spaces = 1)))
+  }, images)
+
+}
+
+
+
+text_list <-  multi_text_clean(multi_images1)
+
+
+text_list <-  multi_text_clean(multi_images)
+
+
+(text_multi <- stringr::str_split(text_list, pattern = "\\s{5,}"))
+
+#need to set name before 
+names(text_multi) <- "Text"
+
+t <- map_df(text_multi, ~.x)
+
+View(t)
 
 readr::write_file(text, "./test-data/pg1_test1.txt")
 
-wb <- createWorkbook()
-
-addWorksheet(wb, "Page 1", gridLines = FALSE)
-
-writeData(wb, sheet = 1, t, rowNames = FALSE)
-
-saveWorkbook(wb, "test_ocr_conversion.xlsx", overwrite = TRUE)
-
-#reprex size
-
-image <- image_read("./test-data/page_1.png")
-
-text <- image %>%
-  image_crop(geometry_area(width = 1220, height = 900,
-                           y_off = 260, x_off = 355)) %>% 
-  image_resize("2000x") %>%
-  image_convert(type = 'Grayscale') %>%
-  image_trim(fuzz = 40) %>%
-  image_write(format = 'png', density = '300x300') %>%
-  tesseract::ocr() 
-
-stringr::str_split(text, pattern = "\n")
+print_xlsx <- function(df){
+  
+  wb <- createWorkbook()
+  addWorksheet(wb, "Page 1", gridLines = FALSE)
+  writeData(wb, sheet = 1, t, rowNames = FALSE)
+  saveWorkbook(wb, "test_ocr_conversion.xlsx", overwrite = TRUE)
+  
+  
+}
